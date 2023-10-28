08ccaa833db91c9ff064cb0cdf371079d5f5ba88
Yudai Nakajima <kituneudon97@gmail.com>
2023-10-28 17:46:20 +0900

    作業途中だけど記録用にコミット

--- lib/cdk-autoscaling-stack.ts
+++ lib/cdk-autoscaling-stack.ts
@@ -1,5 +1,6 @@
 import * as cdk from 'aws-cdk-lib';
 import * as ec2 from 'aws-cdk-lib/aws-ec2';
+import * as elb from 'aws-cdk-lib/aws-elasticloadbalancingv2';
 import { Construct } from 'constructs';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
@@ -7,8 +8,13 @@ export class CdkAutoscalingStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const vpc = new ec2.Vpc(this, 'main-vpc', {
+    const vpc = new ec2.Vpc(this, 'CdkMainVpc', {
       ipAddresses: ec2.IpAddresses.cidr('10.0.0.0/16')
+    });
+
+    const alb = new elb.ApplicationLoadBalancer(this, 'ALB', {
+      vpc,
+
     })
   }
 }
